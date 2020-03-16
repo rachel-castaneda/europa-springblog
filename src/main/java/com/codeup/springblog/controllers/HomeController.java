5f10875b67b0dcca4bfefc6db5cba06012dddb06
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-03-16 10:34:45 -0500

    worked on roll dice

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -18,4 +18,27 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping(path = "/roll-dice")
+    public String rollDice() {
+        return "dice/roll_dice";
+    }
+
+    @GetMapping(path = "roll-dice/{guess}")
+    public String rollDiceGuess(@PathVariable int guess, Model model) {
+        String message;
+
+        int random = (int) Math.ceil(Math.random()* 6);
+        if (random == guess) {
+            message = "you guessed the random number!";
+        } else {
+            message = "sorry, try again";
+        }
+
+        model.addAttribute("message", message);
+        model.addAttribute("guess", guess);
+        model.addAttribute("random", random);
+
+        return "dice/roll-dice-guess";
+    }
+
 }
