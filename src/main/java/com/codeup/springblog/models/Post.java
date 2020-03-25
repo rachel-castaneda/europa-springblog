3b21707391509171ca8164ea6336023ed1c5cbae
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-03-25 15:03:16 -0500

    worked on posts

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,4 +1,47 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Post() { }
+
+    public Post(long id, String title, String body){
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public long getId() { return id; }
+
+    public void setId(long id) { this.id = id; }
+
+    public String getTitle(){ return this.title; }
+
+    public String getBody(){ return this.body; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public void setBody(String body) { this.body = body; }
+
+    public User getUser() { return user; }
+
+    public void setUser(User user) { this.user = user; }
+
 }
