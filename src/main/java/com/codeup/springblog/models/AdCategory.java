0bafb39753a42e09159144905ace651c979a4b32
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-03-25 14:55:56 -0500

    worked on category

--- src/main/java/com/codeup/springblog/models/AdCategory.java
+++ src/main/java/com/codeup/springblog/models/AdCategory.java
@@ -1,4 +1,52 @@
 package com.codeup.springblog.models;
 
+import org.springframework.data.annotation.TypeAlias;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
 public class AdCategory {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToMany()
+    @JoinTable(
+            name="categories_ads",
+            joinColumns = {@JoinColumn(name="category_id")},
+            inverseJoinColumns = {@JoinColumn(name="ad_id")}
+    )
+    private List<Ad> ads;
+
+    public AdCategory() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ad> getAds() {
+        return ads;
+    }
+
+    public void setAds(List<Ad> ads) {
+        this.ads = ads;
+    }
 }
