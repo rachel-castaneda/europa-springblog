e7110b59774eb147f18e868e5f67078df73e83a2
Rachel Castaneda <rachel.castaneda03@gmail.com>
2020-03-25 14:59:03 -0500

    worked on ad_image

--- src/main/java/com/codeup/springblog/models/AdImage.java
+++ src/main/java/com/codeup/springblog/models/AdImage.java
@@ -1,4 +1,33 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ad_images")
 public class AdImage {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String path;
+
+    @ManyToOne
+    @JoinColumn(name = "ad_id")
+    private Ad ad;
+
+    public AdImage() { }
+
+    public long getId() { return id; }
+
+    public void setId(long id) { this.id = id; }
+
+    public String getPath() { return path; }
+
+    public void setPath(String path) { this.path = path; }
+
+    public Ad getAd() { return ad; }
+
+    public void setAd(Ad ad) { this.ad = ad; }
+
 }
